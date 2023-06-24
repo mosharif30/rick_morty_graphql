00835e5aaa7ad45c1ad7f65225144ca65af09ff3
Mohammad Reza SharifKhani <104066048+mosharif30@users.noreply.github.com>
2023-06-24 11:22:21 +0330

    changing router confug

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SingleCharacter from "./components/SingleCharacter";
+import Characters from "./components/Characters.tsx";
+import { QueryClient, QueryClientProvider } from "react-query";
+import "./App.css";
 
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Characters />,
+  },
+  { path: "/characters/:id", element: <SingleCharacter /> },
+]);
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
